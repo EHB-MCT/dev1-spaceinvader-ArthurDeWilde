07005ec1701e6558e8684a409b453835744d3acb
Arthur De Wilde <arthur.de.wilde@student.ehb.be>
2022-10-10 15:59:46 +0200

    Space_invader_arthur_de_wilde

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,22 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawSpaceInvader();
+
+function drawSpaceInvader() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.fillStyle = "#fffff";
+    context.fillRect(0, 0, 300, 300);
+
+    context.fillStyle = "#39FF14";
+    context.fillRect(125, 30, 50, 50);
+    context.fillRect(75, 80, 50, 50); /*(x,y,lengte,hoogte)*/
+    context.fillRect(25, 130, 100, 50);
+    context.fillRect(175, 80, 50, 50);
+    context.fillRect(175, 130, 100, 50);
+    context.fillRect(25, 180, 250, 50);
+}
